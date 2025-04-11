0a0caf9350949fd8eac38ec312b00de0f2c2a975
harshranjan0007 <harshranjan0007@gmail.com>
2025-04-11 22:44:42 +0530

    github data added

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 
+const data = {
+  'text': "hjhgasdyufvwhj ksgdvw ",
+  name: "Alice",
+age: 25,
+city: "London",
+customer_id: 12345,
+customer_name: "Charlie",
+order_total: 50.00,
+}
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -15,8 +25,11 @@ app.get('/login',(req, res) => {
 app.get('/youtube',(req, res) => {
     res.send('<h2>youtube</h2>')
 })
+app.get('/github',(req, res) => {
+    res.json(data)
+})
 
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
